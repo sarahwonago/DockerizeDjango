43be5425afb104f0fcef73eb2cc6e51bdbd48df7
sarahwonago <sarahshaaleh@gmail.com>
2024-10-16 15:34:41 +0300

    setup

---
+++ myapp/views.py
@@ -0,0 +1,6 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET"])
+def home_view(request):
+    return Response({"message": "Hello, we are dockerizing Django!"})
